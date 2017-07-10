eecc4f8d4cb54e077b30d460b07c41a059ebafbc
Rob Soule <rsoule@gmail.com>
2017-07-10 09:30:27 -0400

    Initial checkin

--- README.md
+++
@@ -1,1 +0,0 @@
-# robs-css-boilerplate
---
+++ config.rb
@@ -0,0 +1,15 @@
+# Require any additional compass plugins here.
+
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "./css"
+sass_dir = "./css/scss"
+images_dir = "./images"
+javascripts_dir = "./js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compressed # :compressed for PROD  :expanded for LOCAL
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
